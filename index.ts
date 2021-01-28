e788fdeeb9459a17f2901134607dc52f96717de8
Francesco Lentini <francesco.lentini@gmail.com>
2021-01-28 12:01:57 +0000

    replace child_process.exec with tmp package
    to create temp scenario file

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
 import express from "express";
+import tmp from "tmp";
 import ioserver from "socket.io";
 import { createServer } from "http";
-import { promisify } from "util";
 import { createWriteStream } from "fs";
 import * as childProcess from "child_process";
 
-const execPromise = promisify(childProcess.exec);
 const app = express();
 const http = createServer(app);
 const io = new ioserver.Server(http);
@@ -13,20 +12,25 @@ const PORT = 9001;
 
 let child: childProcess.ChildProcess;
 
+enum ArtilleryStatus {
+  READY = 0,
+  RUNNING = 1,
+}
+
 app.use(express.static("public"));
 
 app.post("/run", async (req, res) => {
   try {
-    const { stdout } = await execPromise("mktemp");
-    const tempFile = stdout.trim();
-    const scenarioFile = createWriteStream(tempFile);
+    const tempFile = tmp.fileSync();
+    const scenarioFile = createWriteStream(tempFile.name);
 
     req.pipe(scenarioFile).on("finish", () => {
       if (child?.exitCode === null) {
         child.kill("SIGINT");
       }
 
-      child = childProcess.spawn("artillery", ["run", tempFile]);
+      child = childProcess.spawn("artillery", ["run", tempFile.name]);
+      io.emit("artilleryStatus", ArtilleryStatus.RUNNING);
 
       if (child.stdout) {
         child.stdout.on("data", (output) => {
